7cf31ef7b97e85f0c860891d5c22eed281b1cabd
dylandhall <dylandhall@users.noreply.github.com>
2024-06-14 13:53:15 +0000

    deploy: 107ddca4b66a73b08b0884d3b6b3470fbb64e589

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -17,7 +17,7 @@ const RESOURCES = {"canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b
 "icons/Icon-512.png": "70ba01667b87f98815ccbb8ad996b05a",
 "manifest.json": "1b23ef4446db70a5bce840bb8887a36f",
 "favicon.png": "78f5f9a22f4531f19021ffcc29d0c3c6",
-"flutter_bootstrap.js": "e19ba729554124b54e330c5b0a4369f6",
+"flutter_bootstrap.js": "06026a383041950e5c35b338882d4b79",
 "version.json": "1e3ba38571309cf0b6000c67fcf45d05",
 "index.html": "ff67c328fe05f7159774a21f0774fa2e",
 "/": "ff67c328fe05f7159774a21f0774fa2e",
